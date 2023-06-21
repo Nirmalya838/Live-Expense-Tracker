4319ab44e7e039e66da360e9f97539262aa1282c
Nirmalya838 <122223948+Nirmalya838@users.noreply.github.com>
2023-06-21 14:06:08 +0530

    LogOut enabled & LeaderBoard Optimized

--- controllers/getLeaderDetailsController.js
+++ controllers/getLeaderDetailsController.js
@@ -1,17 +1,3 @@
-// const Expense = require('../models/expense');
-// const User = require('../models/user');
-
-// exports.getAllUsers = async (req, res) => {
-//   try {
-//     const users = await User.findAll();
-
-//     res.status(200).json(users);
-//   } catch (error) {
-//     console.error('Error retrieving users:', error);
-//     res.status(500).json({ message: 'Internal Server Error' });
-//   }
-// };
-
 const Expense = require('../models/expense');
 const User = require('../models/user');
 const { Op } = require('sequelize');
@@ -26,13 +12,13 @@ exports.getAllUsers = async (req, res) => {
         {
           model: Expense,
           attributes: [
-            'id', // Include the 'id' column
+            'id', 
             [sequelize.fn('SUM', sequelize.col('amount')), 'totalExpense']
           ],
           as: 'expenses',
         },
       ],
-      group: ['user.id', 'expenses.id'], // Include the 'expenses.id' column in the GROUP BY clause
+      group: ['user.id', 'expenses.id'], 
     });
 
     res.status(200).json(users);
